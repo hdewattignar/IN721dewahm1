2a7f7c2e1411f953690ac76ee4ecd77d0095b47b
hdewattignar <hdewattignar@gmail.com>
2016-03-17 19:46:22 +1300

    working

--- RequestData/app/src/main/java/bit/dewahm1/requestdata/Settings.java
+++ RequestData/app/src/main/java/bit/dewahm1/requestdata/Settings.java
@@ -1,7 +1,11 @@
 package bit.dewahm1.requestdata;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //get the text view
+        TextView settingsTextView = (TextView) findViewById(R.id.tv_settingsText);
+
+        //make an intent. get the text colour and pass it back
+        Intent returnColour = new Intent();
+        int textColour = settingsTextView.getCurrentTextColor();
+        returnColour.putExtra("colourResult", textColour);
+        setResult(Activity.RESULT_OK, returnColour);
+        finish();
     }
 }
