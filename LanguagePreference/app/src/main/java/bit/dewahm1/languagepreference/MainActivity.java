041128e60570fc1ba0e3bbca55da4119bcefb2da
hdewattignar <hdewattignar@gmail.com>
2016-04-06 14:13:17 +1200

    Language preference

---
+++ LanguagePreference/app/src/main/java/bit/dewahm1/languagepreference/MainActivity.java
@@ -0,0 +1,13 @@
+package bit.dewahm1.languagepreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
