230e41181f07700b1411d6847d5809004b801c3d
hdewattignar <hdewattignar@gmail.com>
2016-04-12 20:43:39 +1200

    first commit
    UI done

---
+++ FormatedFileIO/app/src/main/java/bit/dewahm1/formatedfileio/MainActivity.java
@@ -0,0 +1,21 @@
+package bit.dewahm1.formatedfileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String JSONDunedinEvents = "dunedin_events.json";
+
+    String JSONinput = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
