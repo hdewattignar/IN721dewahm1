5d4af47092a4ef1f5302635a30c52128db5ac035
hdewattignar <hdewattignar@gmail.com>
2016-04-12 20:47:16 +1200

    fixed merge conflict

--- FormatedFileIO/app/src/main/java/bit/dewahm1/formatedfileio/MainActivity.java
+++ FormatedFileIO/app/src/main/java/bit/dewahm1/formatedfileio/MainActivity.java
@@ -5,24 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
+
     public static final String JSONDunedinEvents = "dunedin_events.json";
 
     String JSONinput = "";
 
-=======
->>>>>>> origin/master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
 
 
-        
-
-
-=======
->>>>>>> origin/master
     }
 }
