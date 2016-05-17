ec7a4c74eedd6db665a149b5a0dcd68a4ae2ecb7
hdewattignar <hdewattignar@gmail.com>
2016-05-17 17:08:05 +1200

    working

--- Accelerometer/app/src/main/java/bit/dewahm1/accelerometer/MainActivity.java
+++ Accelerometer/app/src/main/java/bit/dewahm1/accelerometer/MainActivity.java
@@ -6,13 +6,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     SensorManager sensorManager;
     Sensor accelerometer;
-    ImageView image;
+    ImageView ballImage;
+    DisplayMetrics displayMetrics;
+    float ballX;
+    float ballY;
+    int imageOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,16 @@ public class MainActivity extends AppCompatActivity {
 
         sensorManager = (SensorManager)MainActivity.this.getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        image = (ImageView)findViewById(R.id.imageView);
-        image.setImageResource(R.drawable.golf_ball);
+
+        ballImage = (ImageView)findViewById(R.id.imageView_golfBall);
+        ballImage.setImageResource(R.drawable.golf_ball);
+
+        displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        ballX = 50;
+        ballY = 50;
+        imageOffset = 50;
     }
 
     public class accelerometerHandler implements SensorEventListener
@@ -34,11 +47,25 @@ public class MainActivity extends AppCompatActivity {
             float accelY = event.values[1];
             float accelZ = event.values[2];
 
-            float x = image.getX() - (accelX * 3);
-            float y = (image.getY() + accelY) * 3;
+            ballX = ballImage.getX() - (accelX * 3);
+            ballY = ballImage.getY() + (accelY * 3);
 
-            image.setX(x);
-            image.setY(y);
+            checkBoundries();
+
+            ballImage.setX(ballX);
+            ballImage.setY(ballY);
+        }
+
+        public void checkBoundries()
+        {
+            if(ballX < 0)
+                ballX = 0;
+            if(ballY < 0)
+                ballY = 0;
+            if(ballX > displayMetrics.widthPixels)
+                ballX = displayMetrics.widthPixels - imageOffset;
+            if(ballY > displayMetrics.heightPixels)
+                ballY = displayMetrics.heightPixels - imageOffset;
         }
 
         @Override
